b1fd1744964a483aa40b466799abd5785cf3ca2e
megannn3 <possiblymegansinclair@gmail.com>
2024-03-11 10:32:53 -0700

    attempted to make alerts work

--- big_project/alert.gd
+++ big_project/alert.gd
@@ -1,5 +1,5 @@
 extends Area2D
-
+signal hit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +9,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_body_entered(body):
+	hit.emit()
+	get_tree().change_scene_to_file("res://Info.tscn")
+	$CollisionShape2D.set_deferred("disabled", true)
+	pass # Replace with function body.
